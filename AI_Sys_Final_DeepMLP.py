156b7b82d65d6c47b9829581c8610eab9a7fd893
hms1205 <67571328+hms1205@users.noreply.github.com>
2021-06-04 23:00:00 +0900

    reshape update

--- AI_Sys_Final_DeepMLP.py
+++ AI_Sys_Final_DeepMLP.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -7,8 +8,8 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 
 # train.csv, test.csv PATH
-df=pd.read_csv(r"C:\Users\U310\Downloads\archive\train.csv")
-test=pd.read_csv(r"C:\Users\U310\Downloads\archive\test.csv")
+df=pd.read_csv(r"train.csv")
+test=pd.read_csv(r"test.csv")
 
 yt=np.array(df['price_range'])
 xt=df.drop(['price_range'], axis=1)
@@ -17,18 +18,16 @@ xt=np.array(xt)
 scaler=MinMaxScaler()
 xt=scaler.fit_transform(xt)
 
-# x_train,x_test,y_train,y_test = train_test_split(xt, yt, test_size=0.2, random_state=42)
+x_train,x_test,y_train,y_test = train_test_split(xt, yt, test_size=0.2, random_state=42)
 
-(x_train, y_train) = xt
-(x_test, y_test) = yt
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = xtrain/255.0
-x_test = xtest/255.0
+x_train = x_train.reshape(2000, 16)
+x_test = x_test.reshape(2000, 4)
+x_train = x_train/255.0
+x_test = x_test/255.0
 y_train = tf.keras.utils.to_categorical(y_train, 10)
 y_test = tf.keras.utils.to_categorical(y_test, 10)
 
-n_input = 784
+n_input = 16
 n_hidden1 = 1024
 n_hidden2 = 512
 n_hidden3 = 512
